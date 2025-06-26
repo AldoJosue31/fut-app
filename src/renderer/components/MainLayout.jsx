9645f6cf4c74b4a1a3dd5bd0248dc52b275681b6
Aldo Josue <caldo3108@gmail.com>
2025-06-25 23:42:36 -0600

    SBV1

--- src/renderer/components/MainLayout.jsx
+++ src/renderer/components/MainLayout.jsx
@@ -1,21 +1,37 @@
 // src/components/MainLayout.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import CurrentRouteLogger from "./CurrentRouteLogger";
 import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
+  // Estado levantado aquí:
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  // estilo dinámico para el contenido
+  const contentStyle = {
+    marginLeft: isSidebarOpen ? '250px' : '70px',
+    transition: 'margin-left 0.3s ease-in-out',
+    padding: '20px',
+    backgroundColor: '#2D2D2D',
+    color: 'white',
+    height: '100vh',
+    overflow: 'auto',
+  };
+
   return (
     <div>
       <CurrentRouteLogger />
-      {  <div style={{ display: 'flex' }}>
-      <Sidebar />
-      <div style={{ flex: 1, padding: '20px', backgroundColor: '#2D2D2D', color: 'white'}}>
-        <Outlet /> {/* Este es el espacio donde se cargarán las páginas */}
+      <div>
+        <Sidebar
+          isOpen={isSidebarOpen}
+          onToggle={() => setIsSidebarOpen(o => !o)}
+        />
+        <div style={contentStyle}>
+          <Outlet />
+        </div>
       </div>
-    </div>}
     </div>
-
   );
 };
 
